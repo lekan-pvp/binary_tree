d7954f8634c619112e55dcc49f182c398307f933
lekan <lekan.lekan@gmail.com>
2021-06-28 12:48:51 +0300

    height of binary tree

--- main.py
+++ main.py
@@ -148,6 +148,15 @@ class BinaryTree(object):
 
         return traversal
 
+    def height(self, node):
+        if node is None:
+            return -1
+
+        left_height = self.height(node.left)
+        right_height = self.height(node.right)
+        return 1 + max(left_height, right_height)
+
+
 tree = BinaryTree(1)
 tree.root.left = Node(2)
 tree.root.right = Node(3)
